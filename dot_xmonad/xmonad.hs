13d82234e037fe0193eddb391e5edea3ffbfed9b
Kevin Qiu <kevin@freshbooks.com>
2012-06-25 21:30:02 -0400

    more keybindings

--- dot_xmonad/xmonad.hs
+++ dot_xmonad/xmonad.hs
@@ -1,6 +1,9 @@
+import System.IO
+import System.Exit
 import XMonad
 import XMonad.Config.Gnome
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
 myManageHook = composeAll (
     [ manageHook gnomeConfig
@@ -10,7 +13,58 @@ myManageHook = composeAll (
 
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig { XMonad.modMask = modMask }) = M.fromList $
-    [ ((modMask, xK_w), spawn "google-chrome") ]
+    [ ((modMask .|. shiftMask, xK_Return),
+       spawn $ XMonad.terminal conf)
+    , ((modMask, xK_p),
+       spawn "Unity-2d-shell")
+    , ((modMask, xK_o),
+      spawn "google-chrome")
+    -- Standard xmonad key bindings
+    , ((modMask .|. shiftMask, xK_c),
+      kill)
+    , ((modMask, xK_space),
+      sendMessage NextLayout)
+    , ((modMask .|. shiftMask, xK_space),
+      setLayout $ XMonad.layoutHook conf)
+    , ((modMask, xK_n),
+      refresh)
+    , ((modMask, xK_Tab),
+      windows W.focusDown)
+    , ((modMask, xK_j),
+      windows W.focusDown)
+    , ((modMask, xK_k),
+      windows W.focusUp)
+    , ((modMask, xK_m),
+      windows W.focusMaster)
+    , ((modMask, xK_Return),
+      windows W.swapMaster)
+    , ((modMask .|. shiftMask, xK_j),
+      windows W.swapDown)
+    , ((modMask .|. shiftMask, xK_k),
+      windows W.swapUp)
+    , ((modMask, xK_h),
+      sendMessage Shrink)
+    , ((modMask, xK_l),
+      sendMessage Expand)
+    , ((modMask, xK_t),
+      withFocused $ windows . W.sink)
+    , ((modMask, xK_comma),
+      sendMessage (IncMasterN 1))
+    , ((modMask, xK_period),
+      sendMessage (IncMasterN (-1)))
+    -- Quit
+    , ((modMask .|. shiftMask, xK_q),
+      io (exitWith ExitSuccess))
+    -- Restart xmonad
+    , ((modMask, xK_q),
+      restart "xmonad" True)
+    ]
+    ++
+    -- mod-[1..9], switch to workspace N
+    -- mod-shift-[1..9], move client to workspace N
+    [ ((m .|. modMask, k), windows $ f i)
+         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
 main = xmonad gnomeConfig
     { manageHook = myManageHook
