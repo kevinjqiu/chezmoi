92760029ad3ebce18a9da2ba2c9675319bf1d7f0
Kevin Qiu <kevin@freshbooks.com>
2013-08-11 12:12:38 -0400

    Add battery info

--- dot_xmonad/xmobar.hs
+++ dot_xmonad/xmobar.hs
@@ -8,9 +8,15 @@ Config { font = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*"
                     , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
+                    , Run BatteryP ["BAT0"]
+                                   ["-t", "<acstatus><watts> (<left>%)",
+                                    "--", "-O", "<fc=green>On</fc> -", "-i", "",
+                                    "-L", "-15", "-H", "-5",
+                                    "-l", "red", "-m", "blue", "-h", "green"]
+                                   600
                     , Run StdinReader
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% } { %cpu% | %memory% | %enp0s25% - %wlp3s0% | <fc=#ee9a00>%date%</fc>| %whoami%"
+       , template = "%StdinReader% } { %cpu% | %memory% | %battery% | %enp0s25% - %wlp3s0% | <fc=#ee9a00>%date%</fc>| %whoami%"
        }
