5ba9f309e1628e254edefa9c16017fe189d063df
Kevin Qiu <kevin@freshbooks.com>
2014-03-04 15:57:57 -0500

    Fix script

--- vim-bootstrap.sh
+++ vim-bootstrap.sh
@@ -15,7 +15,7 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do
 done
 
 git clone --recursive $REPO_URL $ENDPATH
-mkdir -p $ENDPATH/.vim/bundle
+mkdir -p $ENDPATH/vim/bundle
 ln -s $ENDPATH/vimrc $HOME/.vimrc
 ln -s $ENDPATH/vim $HOME/.vim
 
